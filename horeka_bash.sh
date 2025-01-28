f6b332c5bf477f54e7b39ce24844a2f04bd0ec9f
Hongyi <wolfgangzhou1996@outlook.com>
2025-01-28 20:18:37 +0100

    update config and bash

---
+++ horeka_bash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -p accelerated # accelerated-h100 # dev_accelerated
+#SBATCH -A hk-project-sustainebot
+#SBATCH -J DP-training
+
+# Cluster Settings
+#SBATCH -n 1       # Number of tasks
+#SBATCH -c 16  # Number of cores per task
+#SBATCH -t 12:00:00 # 30:00 # 2-00:00:00 # 1:00:00 # 2-00:00:00 ## # 06:00:00 # 1-00:30:00 # 2-00:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+
+export MUJOCO_GL=egl
+
+python /hkfs/work/workspace/scratch/lx7270-flower/Codes/lerobot/lerobot/scripts/train.py
